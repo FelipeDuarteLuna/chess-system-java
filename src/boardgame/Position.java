917555bf26fbe285055eefa333153e2d273242b9
FelipeDuarteLuna <felipinho.luna@gmail.com>
2020-05-06 06:46:59 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,41 @@
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	//Construthores
+	public Position() {
+		
+	}
+
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	//Getter's e Setter's
+	public void setRow(int linha) {
+		this.row = linha;
+	}
+	
+	public int getRow() {
+		return this.row;
+	}
+	
+	public void setColumn(int coluna) {
+		this.column= coluna;
+	}
+	
+	public int getColumn() {
+		return this.column;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.getRow() + ", " + this.getColumn();
+	}
+
+}
